c6b30c842c5aa2727a0621ef0b37b7a040ab451c
lkwjohn <lkwjohn@yahoo.com>
2017-03-27 20:24:36 +0800

    init commit

---
+++ jap2eng.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "translate_japanese":
+        return {}
+    baseurl = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyAhF49eTdOK088ldtFFkqEGt50FzWXSVoc&source=jp&target=en&"
+    translate_query = makeTranslateQuery(req)
+    if translate_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': translate_query}) 
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeTranslateQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    japanese = parameters.get("japanese")
+    if japanese is None:
+        return None
+
+    return japanese
+
+
+def makeWebhookResult(data):
+    query = data.get('data')
+    if query is None:
+        return {}
+
+    result = query.get('translations')
+    if result is None:
+        return {}
+
+    translatedText = result.get('translatedText')
+    if translatedText is None:
+        return {}
+
+
+    # print(json.dumps(item, indent=4))
+
+    speech = translatedText
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "translate_japanese"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
